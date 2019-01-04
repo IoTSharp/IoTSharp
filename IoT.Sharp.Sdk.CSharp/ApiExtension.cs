6ea8db2b4863c300330df8c66e63052a477517c5
MysticBoy <mysticboy@live.com>
2019-01-04 12:49:09 +0800

    add c# client api

---
+++ IoT.Sharp.Sdk.CSharp/ApiExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT.Sharp.Sdk.CSharp
+{
+    public static class ApiExtension
+    {
+       public  static ApiResult ToResult(this SwaggerException swaggerException)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<ApiResult>(swaggerException.Response);
+        }
+    }
+}
---
+++ IoT.Sharp.Sdk.CSharp/Partials.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT.Sharp.Sdk.CSharp
+{
+    public class ApiResult
+    {
+        public int code { get; set; }
+        public string msg { get; set; }
+        public JObject data { get; set; }
+    }
+
+    public partial class AccountClient
+    {
+        public AccountClient(string url) : this(new System.Net.Http.HttpClient())
+        {
+            BaseUrl = url;
+        }
+    }
+
+}
