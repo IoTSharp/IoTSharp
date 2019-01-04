5673d1e6a27b5eccb883bc98844a659abdb2ce2c
MysticBoy <mysticboy@live.com>
2019-01-05 00:39:06 +0800

    add other api

---
+++ IoTSharp.Hub/Controllers/DevicesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IoTSharp.Hub.Data;
+
+namespace IoTSharp.Hub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DevicesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Devices
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Device>>> GetDevice()
+        {
+            return await _context.Device.ToListAsync();
+        }
+
+        // GET: api/Devices/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Device>> GetDevice(Guid id)
+        {
+            var device = await _context.Device.FindAsync(id);
+
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            return device;
+        }
+
+        // PUT: api/Devices/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDevice(Guid id, Device device)
+        {
+            if (id != device.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(device).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DeviceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Devices
+        [HttpPost]
+        public async Task<ActionResult<Device>> PostDevice(Device device)
+        {
+            _context.Device.Add(device);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDevice", new { id = device.Id }, device);
+        }
+
+        // DELETE: api/Devices/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Device>> DeleteDevice(Guid id)
+        {
+            var device = await _context.Device.FindAsync(id);
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            _context.Device.Remove(device);
+            await _context.SaveChangesAsync();
+
+            return device;
+        }
+
+        private bool DeviceExists(Guid id)
+        {
+            return _context.Device.Any(e => e.Id == id);
+        }
+    }
+}
