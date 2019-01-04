13f7370ed743c7203b2516ec9c7affcf9f423215
MysticBoy <mysticboy@live.com>
2019-01-04 16:22:14 +0800

    add  modify relationship ,add installer api

--- IoTSharp.Hub/Data/Migrations/20190104081406_ModifyRelationship.Designer.cs
+++ IoTSharp.Hub/Data/Migrations/20190104081406_ModifyRelationship.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using IoTSharp.Hub.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace IoTSharp.Hub.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190104081406_ModifyRelationship")]
+    partial class ModifyRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- IoTSharp.Hub/Data/Relationship.cs
+++ IoTSharp.Hub/Data/Relationship.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,11 @@ namespace IoTSharp.Hub.Data
 {
     public class Relationship
     {
+
         [Key]
         public Guid Id { get; set; }
-        public virtual IdentityUser Identity { get; set; }
+        public IdentityUser IdentityUser  { get; set; }
         public virtual Tenant Tenant { get; set; }
-
         public virtual Customer Customer { get; set; }
     }
 }
