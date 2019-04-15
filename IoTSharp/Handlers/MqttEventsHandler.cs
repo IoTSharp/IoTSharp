377b83846a2a3726afe839df6a9e0d761b1ad7b9
MysticBoy <mysticboy@live.com>
2019-04-15 11:49:53 +0800

    add mqttclient    , telemetry and attributes upload by mqtt

--- IoTSharp/Handlers/MqttEventsHandler.cs
+++ IoTSharp/Handlers/MqttEventsHandler.cs
@@ -1,4 +1,5 @@
 ﻿using IoTSharp.Data;
+using IoTSharp.Extensions;
 using IoTSharp.X509Extensions;
 using Microsoft.Extensions.Logging;
 using MQTTnet;
@@ -16,12 +17,12 @@ namespace IoTSharp.Handlers
 {
     public class MqttEventsHandler
     {
-        ILogger<MqttEventsHandler> Logger { get; set; }
+        readonly ILogger<MqttEventsHandler> _logger ;
         readonly ApplicationDbContext  _dbContext;
         readonly IMqttServerEx _serverEx;
-        public MqttEventsHandler(ILogger<MqttEventsHandler> _logger, ApplicationDbContext  dbContext,IMqttServerEx serverEx )
+        public MqttEventsHandler(ILogger<MqttEventsHandler> logger, ApplicationDbContext  dbContext,IMqttServerEx serverEx )
         {
-            Logger = _logger;
+            _logger = logger;
             _dbContext = dbContext;
             _serverEx = serverEx;
         }
@@ -29,26 +30,26 @@ namespace IoTSharp.Handlers
         static long clients = 0;
         internal void Server_ClientConnected(object sender, MqttClientConnectedEventArgs e)
         {
-            Logger.LogInformation($"Client [{e.ClientId}] connected");
+            _logger.LogInformation($"Client [{e.ClientId}] connected");
             clients++;
             Task.Run(() => _serverEx.PublishAsync("$SYS/broker/clients/total", clients.ToString()));
         }
         static DateTime uptime = DateTime.MinValue;
         internal void Server_Started(object sender, EventArgs e)
         {
-            Logger.LogInformation($"MqttServer is  started");
+            _logger.LogInformation($"MqttServer is  started");
             uptime = DateTime.Now;
         }
 
         internal void Server_Stopped(object sender, EventArgs e)
         {
-            Logger.LogInformation($"Server is stopped");
+            _logger.LogInformation($"Server is stopped");
         }
         Dictionary<string, int> lstTopics = new Dictionary<string, int>();
         long received = 0;
         internal void Server_ApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
         {
-            Logger.LogInformation($"Server received {e.ClientId}'s message: Topic=[{e.ApplicationMessage.Topic }],Retain=[{e.ApplicationMessage.Retain}],QualityOfServiceLevel=[{e.ApplicationMessage.QualityOfServiceLevel}]");
+            _logger.LogInformation($"Server received {e.ClientId}'s message: Topic=[{e.ApplicationMessage.Topic }],Retain=[{e.ApplicationMessage.Retain}],QualityOfServiceLevel=[{e.ApplicationMessage.QualityOfServiceLevel}]");
             if (!lstTopics.ContainsKey(e.ApplicationMessage.Topic))
             {
                 lstTopics.Add(e.ApplicationMessage.Topic, 1);
@@ -58,12 +59,13 @@ namespace IoTSharp.Handlers
             {
                 lstTopics[e.ApplicationMessage.Topic]++;
             }
+       
             received += e.ApplicationMessage.Payload.Length;
         }
         long Subscribed;
         internal void Server_ClientSubscribedTopic(object sender, MqttClientSubscribedTopicEventArgs e)
         {
-            Logger.LogInformation($"Client [{e.ClientId}] subscribed [{e.TopicFilter}]");
+            _logger.LogInformation($"Client [{e.ClientId}] subscribed [{e.TopicFilter}]");
             if (e.TopicFilter.Topic.StartsWith("$SYS/"))
             {
                 if (e.TopicFilter.Topic.StartsWith("$SYS/broker/version"))
@@ -77,6 +79,14 @@ namespace IoTSharp.Handlers
                     Task.Run(() => _serverEx.PublishAsync("$SYS/broker/uptime", uptime.ToString()));
                 }
             }
+            if (e.TopicFilter.Topic.ToLower().StartsWith("/Telemetry/"))
+            {
+                if (Devices.ContainsKey(e.ClientId))
+                {
+                   
+                  
+                }
+            }
             else
             {
                 Subscribed++;
@@ -88,13 +98,14 @@ namespace IoTSharp.Handlers
 
         internal void Server_ClientUnsubscribedTopic(object sender, MqttClientUnsubscribedTopicEventArgs e)
         {
-            Logger.LogInformation($"Client [{e.ClientId}] unsubscribed[{e.TopicFilter}]");
+            _logger.LogInformation($"Client [{e.ClientId}] unsubscribed[{e.TopicFilter}]");
             if (!e.TopicFilter.StartsWith("$SYS/"))
             {
                 Subscribed--;
                 Task.Run(() => _serverEx.PublishAsync("$SYS/broker/subscriptions/count", Subscribed.ToString()));
             }
         }
+        internal static  Dictionary<string, Device> Devices = new Dictionary<string, Device>();
         public static string MD5Sum(string text) => BitConverter.ToString(MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(text))).Replace("-", "");
         internal void Server_ClientConnectionValidator(object sender, MqttClientConnectionValidatorEventArgs e)
         {
@@ -103,11 +114,11 @@ namespace IoTSharp.Handlers
             if (string.IsNullOrEmpty(obj.Username) && uri.IsLoopback)
             {
                 obj.ReturnCode = MQTTnet.Protocol.MqttConnectReturnCode.ConnectionAccepted;
-                Logger.LogInformation($"Loopback {obj.Endpoint}， ConnectionAccepted");
+                _logger.LogInformation($"Loopback {obj.Endpoint}， ConnectionAccepted");
             }
             else
             {
-                Logger.LogInformation($"ClientId={obj.ClientId},Endpoint={obj.Endpoint},Username={obj.Username}，Password={obj.Password},WillMessage={obj.WillMessage?.ConvertPayloadToString()}");
+                _logger.LogInformation($"ClientId={obj.ClientId},Endpoint={obj.Endpoint},Username={obj.Username}，Password={obj.Password},WillMessage={obj.WillMessage?.ConvertPayloadToString()}");
                 var mcr = _dbContext.DeviceIdentities.FirstOrDefault(mc => (mc.IdentityType == IdentityType.AccessToken && mc.IdentityId == obj.Username) 
                                                                             || (mc.IdentityType== IdentityType.DevicePassword && mc.IdentityId==obj.Username && mc.IdentityValue==obj.Password));
                 if (mcr != null)
@@ -115,11 +126,11 @@ namespace IoTSharp.Handlers
                     try
                     {
                         var device = mcr.Device;
-                      
+                        Devices.Add(e.Context.ClientId, device);
                     }
                     catch (Exception ex)
                     {
-                        Logger.LogError(ex, "ConnectionRefusedServerUnavailable {0}", ex.Message);
+                        _logger.LogError(ex, "ConnectionRefusedServerUnavailable {0}", ex.Message);
                         obj.ReturnCode = MQTTnet.Protocol.MqttConnectReturnCode.ConnectionRefusedServerUnavailable;
                     }
 
@@ -127,7 +138,7 @@ namespace IoTSharp.Handlers
                 else
                 {
                     obj.ReturnCode = MQTTnet.Protocol.MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
-                    Logger.LogInformation($"Bad username or password {obj.Username},connection {obj.Endpoint} refused");
+                    _logger.LogInformation($"Bad username or password {obj.Username},connection {obj.Endpoint} refused");
                 }
             }
 
