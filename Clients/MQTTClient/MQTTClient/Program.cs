c3833c3994549edc01f7a5c72f6fb089fa2fec71
MysticBoy <mysticboy@live.com>
2019-05-24 09:55:06 +0800

    add mqtt client

---
+++ Clients/MQTTClient/MQTTClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace MQTTClient
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MQTTnet.MqttFactory factory = new MQTTnet.MqttFactory();
+            var client = factory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                        .WithClientId(Guid.NewGuid().ToString())
+                        .WithTcpServer("localhost")
+                        .WithCredentials("3cb97cd31fbc40b08d12ec47a6fad622")//token
+                        .Build();
+            Task.Run(async () =>
+            {
+                await client.ConnectAsync(options);
+                do
+                {
+                    var message = new MqttApplicationMessageBuilder()
+                                   .WithTopic("/devices/me/telemetry")
+                                   .WithPayload(JsonConvert.SerializeObject(new
+                                   {
+                                       RandomString = Guid.NewGuid().ToString(),
+                                       NowTime = DateTime.Now
+                                   }))
+                                   .Build();
+                    Console.WriteLine(message.ConvertPayloadToString());
+                    await client.PublishAsync(message);
+                    await Task.Delay(TimeSpan.FromSeconds(10));
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+                await client.DisconnectAsync();
+            }).Wait();
+        }
+    }
+}
---
+++ Clients/MQTTClient/MQTTClient/readme.md
@@ -0,0 +1,15 @@
+﻿### Topic
+
+- Upload telemetry  to device 
+    /devices/me/telemetry
+- Upload telemetry data to gateway's device
+    /devices/devicename/telemetry
+
+- Upload attributes to device 
+    /devices/me/attributes
+- Upload attributes data to gateway's device
+    /devices/devicename/telemetry
+
+
+
+
