4a7b72a2cf2fc55ab059ad537496085ef67747aa
MysticBoy <mysticboy@live.com>
2019-06-10 11:33:03 +0800

    modify for vue admin

--- IoTSharp/Dtos/AccountDto.cs
+++ IoTSharp/Dtos/AccountDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IoTSharp.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -14,6 +15,7 @@ namespace IoTSharp.Dtos
 
     public class LoginResult
     {
+        public ApiCode Code { get; set; }
         public string UserName { get; set; }
         public Microsoft.AspNetCore.Identity.SignInResult SignIn { get; set; }
         public bool Succeeded { get; set; }
--- IoTSharp/Dtos/UserInfoDto.cs
+++ IoTSharp/Dtos/UserInfoDto.cs
@@ -8,6 +8,11 @@ namespace IoTSharp.Dtos
 {
     public class UserInfoDto
     {
+        public ApiCode Code { get; set; }
+        public string Roles {get; set;}
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public string Introduction { get; set; }
         public Customer Customer { get; set; }
         public Tenant Tenant { get; set; }
     }
