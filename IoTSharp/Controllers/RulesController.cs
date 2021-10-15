8e63bc7d7c0df6e3dccea44b3c9186e016604416
wq1234wq <wq1234wq@163.com>
2021-10-15 12:20:44 +0800

    规则下发

--- IoTSharp/Controllers/RulesController.cs
+++ IoTSharp/Controllers/RulesController.cs
@@ -164,20 +164,23 @@ namespace IoTSharp.Controllers
         }
 
         [HttpGet("[action]")]
-        public AppMessage<FlowRule> BindDevice (ModelRuleBind m)
+        public async Task<AppMessage<FlowRule>> BindDevice (ModelRuleBind m)
         {
 
 
-            var profile = this.GetUserProfile();
+            var profile =await this.GetUserProfile();
             if (m.dev != null)
 
             {
                 m.dev.ToList().ForEach(d => {
                     if (_context.DeviceRules.Any(c => c.RuleId == m.rule && c.DeviceId == d)) {
                         var dr = new DeviceRule();
-
-
-
+                        dr.DeviceId = d;
+                        dr.ConfigDateTime=DateTime.Now;
+                        dr.ConfigUser = profile.Id;
+                        dr.RuleId = m.rule;
+                        _context.DeviceRules.Add(dr);
+                        _context.SaveChanges();
                     }
                 
                 });
@@ -188,7 +191,7 @@ namespace IoTSharp.Controllers
             //    return new AppMessage<FlowRule> { ErrType = ErrType.正常返回, Result = rule };
             //}
 
-            return new AppMessage<FlowRule> { ErrType = ErrType.找不到对象, };
+            return new AppMessage<FlowRule> { ErrType = ErrType.参数错误, ErrMessage = "请选择下发设备"};
         }
 
 
