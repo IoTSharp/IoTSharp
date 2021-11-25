a503b428c2e75b6f410b337fda6bb287393795fd
MysticBoy <mysticboy@live.com>
2021-11-25 17:52:09 +0800

    加入 Rin   更便于查看 交互信息 ， https://github.com/mayuki/Rin

--- IoTSharp/Program.cs
+++ IoTSharp/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Silkier.AspNetCore;
 using SilkierQuartz;
-
+using Rin;
 namespace IoTSharp
 {
     public class Program
