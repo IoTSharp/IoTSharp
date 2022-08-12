161d5e6040bf6902fe63b5bf0012897169a1d737
Yanhong.Ma <yanhong.ma@xr-techs.com>
2022-08-12 14:26:52 +0800

    修正没有引用spaproxy导致前端无法直接启动的问题。 调整 ef工具安装在本地项目。

--- tools/ef_db_drop.cmd
+++ tools/ef_db_drop.cmd
@@ -2,4 +2,4 @@ echo off
 cls
 echo Drop  to  IoTSharp.Data.%1
 set ASPNETCORE_ENVIRONMENT=%1
-dotnet ef  database drop      --context IoTSharp.Data.ApplicationDbContext   --startup-project  ..\IoTSharp\IoTSharp.csproj  --project ..\IoTSharp.Data.%1\IoTSharp.Data.%1.csproj
+dotnet dotnet-ef  database drop      --context IoTSharp.Data.ApplicationDbContext   --startup-project  ..\IoTSharp\IoTSharp.csproj  --project ..\IoTSharp.Data.%1\IoTSharp.Data.%1.csproj
