8df3cb044379a0e8cd8df4b85bf586210709c584
MysticBoy <mysticboy@live.com>
2022-09-17 19:16:47 +0800

    将消息事件 移动为单独的项目， 方便下一步重构。

---
+++ IoTSharp.EventBus/EventBusOption.cs
@@ -0,0 +1,21 @@
+﻿using DotNetCore.CAP.Dashboard.NodeDiscovery;
+using MaiKeBing.HostedService.ZeroMQ;
+using Microsoft.Extensions.DependencyInjection;
+using IoTSharp.Contracts;
+
+namespace IoTSharp.EventBus
+{
+    public class EventBusOption
+    {
+        public AppSettings AppSettings { get; set; }
+        public ZMQOption ZMQOption { get; set; }
+        public DiscoveryOptions DiscoveryOptions { get; set; }
+        public string EventBusStore { get; set; }
+        public string EventBusMQ { get; set; }
+        public IHealthChecksBuilder HealthChecks { get; set; }
+
+        public delegate Task RunRulesEventHander(Guid devid, object obj, MountType mountType);
+
+        public  RunRulesEventHander RunRules;
+    }
+}
