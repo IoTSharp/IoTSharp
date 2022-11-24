ac2e24f6deb043745ea31804971c7018b9b382cc
MysticBoy <mysticboy@live.com>
2022-11-24 23:56:21 +0800

    修改用户下拉菜单处为 IoTSharp信息

--- ClientApp/src/layout/navBars/breadcrumb/user.vue
+++ ClientApp/src/layout/navBars/breadcrumb/user.vue
@@ -64,10 +64,10 @@
 			<template #dropdown>
 				<el-dropdown-menu>
 					<el-dropdown-item command="/home">{{ $t('message.user.dropdown1') }}</el-dropdown-item>
-					<el-dropdown-item command="wareHouse">{{ $t('message.user.dropdown6') }}</el-dropdown-item>
 					<el-dropdown-item command="/profile">{{ $t('message.user.dropdown2') }}</el-dropdown-item>
-					<el-dropdown-item command="/404">{{ $t('message.user.dropdown3') }}</el-dropdown-item>
-					<el-dropdown-item command="/401">{{ $t('message.user.dropdown4') }}</el-dropdown-item>
+					<el-dropdown-item command="iotsharp">{{ $t('message.user.dropdown6') }}</el-dropdown-item>
+					<el-dropdown-item command="docs">{{ $t('message.user.dropdown3') }}</el-dropdown-item>
+					<el-dropdown-item command="github">{{ $t('message.user.dropdown4') }}</el-dropdown-item>
 					<el-dropdown-item divided command="logOut">{{ $t('message.user.dropdown5') }}</el-dropdown-item>
 				</el-dropdown-menu>
 			</template>
@@ -166,8 +166,12 @@ export default defineComponent({
 						window.location.reload();
 					})
 					.catch(() => {});
-			} else if (path === 'wareHouse') {
-				window.open('https://gitee.com/lyt-top/vue-next-admin');
+			} else if (path === 'iotsharp') {
+				window.open('https://iotsharp.net/');
+			}  else if (path === 'github') {
+				window.open('https://github.com/IoTSharp');
+			} else if (path === 'docs') {
+				window.open('http://docs.iotsharp.net/');
 			} else {
 				router.push(path);
 			}
