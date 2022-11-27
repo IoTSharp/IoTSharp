d335216018776058e3d523102f35114951c9e979
Kaysar <wacoom89@gmail.com>
2022-11-28 05:48:14 +0800

    style(settings): 默认组件大小调整

--- ClientApp/src/stores/themeConfig.ts
+++ ClientApp/src/stores/themeConfig.ts
@@ -135,7 +135,7 @@ export const useThemeConfig = defineStore('themeConfig', {
 			// 默认初始语言，可选值"<zh-cn|en|zh-tw>"，默认 zh-cn
 			globalI18n: 'zh-cn',
 			// 默认全局组件大小，可选值"<large|'default'|small>"，默认 'large'
-			globalComponentSize: 'large',
+			globalComponentSize: 'default',
 		},
 	}),
 	actions: {
