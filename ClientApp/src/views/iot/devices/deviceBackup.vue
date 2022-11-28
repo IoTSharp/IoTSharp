4951bc03a6228cd9eb69e2368e5db18f523be9e3
Kaysar <wacoom89@gmail.com>
2022-11-28 11:19:11 +0800

    refactor: 组件名修改

--- ClientApp/src/views/iot/devices/deviceBackup.vue
+++ ClientApp/src/views/iot/devices/deviceBackup.vue
@@ -100,7 +100,7 @@
 
 <script lang="ts" setup>
 import {ElMessageBox, ElMessage} from "element-plus";
-import addDevice from "./addDevice.vue";
+import AddDevice from "./AddDevice.vue";
 import {deviceApi} from "/@/api/devices";
 import {Session} from "/@/utils/storage";
 import {appmessage} from "/@/api/iapiresult";
