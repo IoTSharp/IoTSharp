a3655473e89cd58625f6fbb4339d2ee9c69f73a7
qian wang <1510106069@qq.com>
2022-11-28 21:29:50 +0800

    feat: 根证书管理 Close: #9

--- ClientApp/src/views/iot/settings/certmgr.vue
+++ ClientApp/src/views/iot/settings/certmgr.vue
@@ -1,15 +1,42 @@
-<template><div></div></template>
+<template>
+	<el-form :model="form" label-width="120px">
+		<el-form-item label="域名">
+			<el-input v-model="form.domain" />
+		</el-form-item>
+		<el-form-item label="Ca指纹">
+			<el-input v-model="form.caThumbprint" />
+		</el-form-item>
+		<el-form-item label="Broker指纹">
+			<el-input v-model="form.brokerThumbprint" />
+		</el-form-item>
+	</el-form>
+</template>
 
 <script lang="ts">
-import { ref, toRefs, reactive, onMounted, defineComponent } from "vue";
-import { ElMessageBox, ElMessage } from "element-plus";
-
+import { reactive, toRefs, onMounted, defineComponent } from 'vue';
+import { getSysInfo } from '/@/api/installer';
 export default defineComponent({
-  name: 'addDevice',
-  components: {},
-  setup() {
-    onMounted(() => {});
-    return {};
-  },
+	name: 'certification',
+	components: {},
+	setup() {
+		const state = reactive({
+			form: {
+				domain: '',
+				caThumbprint: '',
+				brokerThumbprint: '',
+			},
+		});
+		const getData = () => {
+			getSysInfo().then((res) => {
+				state.form = res.data;
+			});
+		};
+		onMounted(() => {
+			getData();
+		});
+		return {
+			...toRefs(state),
+		};
+	},
 });
 </script>
