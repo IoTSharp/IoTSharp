22cd79f38b99744f6b220c2ccd6ec138b01ea53f
qian wang <1510106069@qq.com>
2022-11-30 23:40:38 +0800

    feat: 初始化根证书

--- ClientApp/src/api/installer/index.ts
+++ ClientApp/src/api/installer/index.ts
@@ -1,14 +1,22 @@
 import request from '/@/utils/request';
-export function getSysInfo(){
-    return request({
-        url: '/api/Installer/Instance',
-        method: 'get'
-    });
+export function getSysInfo() {
+	return request({
+		url: '/api/Installer/Instance',
+		method: 'get',
+	});
 }
-export function initSysAdmin(data:any){
-    return request({
-        url: '/api/Installer/Install',
-        method: 'post',
-        data
-    });
+export function initSysAdmin(data: any) {
+	return request({
+		url: '/api/Installer/Install',
+		method: 'post',
+		data,
+	});
+}
+
+export function initSysCertificate(data?: any) {
+	return request({
+		url: '/api/Installer/CreateRootCertificate',
+		method: 'post',
+		data,
+	});
 }
--- ClientApp/src/views/iot/settings/certmgr.vue
+++ ClientApp/src/views/iot/settings/certmgr.vue
@@ -1,20 +1,17 @@
 <template>
-	<el-form :model="form" label-width="120px">
-		<el-form-item label="域名">
-			<el-input v-model="form.domain" />
-		</el-form-item>
-		<el-form-item label="Ca指纹">
-			<el-input v-model="form.caThumbprint" />
-		</el-form-item>
-		<el-form-item label="Broker指纹">
-			<el-input v-model="form.brokerThumbprint" />
-		</el-form-item>
-	</el-form>
+	<el-card class="box-card">
+		<div class="text item">
+			<el-button type="primary" plain @click="initCertificate">初始化根证书</el-button>
+		</div>
+		<div class="text item"><span class="label">域名: </span>{{ form.domain }}</div>
+		<div class="text item"><span class="label">Ca指纹: </span>{{ form.caThumbprint }}</div>
+		<div class="text item"><span class="label">Broker指纹: </span> {{ form.brokerThumbprint }}</div>
+	</el-card>
 </template>
 
 <script lang="ts">
 import { reactive, toRefs, onMounted, defineComponent } from 'vue';
-import { getSysInfo } from '/@/api/installer';
+import { getSysInfo, initSysCertificate } from '/@/api/installer';
 export default defineComponent({
 	name: 'certification',
 	components: {},
@@ -24,6 +21,10 @@ export default defineComponent({
 				domain: '',
 				caThumbprint: '',
 				brokerThumbprint: '',
+				caCertificate: '',
+				enableTls: false,
+				installed: false,
+				version: '',
 			},
 		});
 		const getData = () => {
@@ -31,12 +32,38 @@ export default defineComponent({
 				state.form = res.data;
 			});
 		};
+		const initCertificate = () => {
+			initSysCertificate().then(() => {
+				getData();
+			});
+		};
 		onMounted(() => {
 			getData();
 		});
 		return {
 			...toRefs(state),
+			initCertificate,
 		};
 	},
 });
 </script>
+
+<style scoped>
+.text {
+	font-size: 14px;
+}
+
+.item {
+	padding: 15px 0;
+}
+
+.label {
+	width: 80px;
+	display: inline-block;
+	text-align: right;
+	margin-right: 10px;
+}
+.box-card {
+	width: 100%;
+}
+</style>
