2b7e1d832444833fa0797e1bfdafbebfce9628d6
MysticBoy <mysticboy@live.com>
2022-12-10 14:03:00 +0800

    修改

--- buildimageuixe.cmd
+++ buildimageuixe.cmd
@@ -1,3 +1,3 @@
-docker build -t registry.uixe.net/uixe/iotsharp  .  -f  IoTSharp/Dockerfile
+docker build -t registry.uixe.net/uixe/iotsharp  .  -f  IoTSharp/Dockerfile_with_vue
 docker push  registry.uixe.net/uixe/iotsharp
 docker image prune -f
