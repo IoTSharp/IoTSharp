703220e42daeddba4d1e3f09b85fc5d9a245bdf8
MysticBoy <mysticboy@live.com>
2022-12-10 23:14:45 +0800

    优化调整健康检查

--- IoTSharp/Controllers/HealthChecksController.cs
+++ IoTSharp/Controllers/HealthChecksController.cs
@@ -14,6 +14,8 @@ using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+using EasyCaching.Core;
 
 namespace IoTSharp.Controllers
 {
@@ -24,11 +26,21 @@ namespace IoTSharp.Controllers
     [ApiController]
     public class HealthChecksController : ControllerBase
     {
+        private readonly IEasyCachingProvider _caching;
+        private readonly JsonSerializerSettings _jsonSerializationSettings;
         private IServiceScopeFactory __serviceScopeFactory;
 
-        public HealthChecksController(IServiceScopeFactory scopeFactor)
+        public HealthChecksController(IServiceScopeFactory scopeFactor, IEasyCachingProviderFactory factory, IOptions<AppSettings> options)
         {
             __serviceScopeFactory = scopeFactor;
+            string _hc_Caching = $"{nameof(CachingUseIn)}-{Enum.GetName(options.Value.CachingUseIn)}";
+            _caching = factory.GetCachingProvider(_hc_Caching);
+            _jsonSerializationSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new[] { new StringEnumConverter() },
+                DateTimeZoneHandling = DateTimeZoneHandling.Local
+            };
         }
         ///<summary>
         /// 获取相关服务的健康检查信息
@@ -38,40 +50,39 @@ namespace IoTSharp.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> Get()
         {
-             var _jsonSerializationSettings = new JsonSerializerSettings
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                Converters = new[] { new StringEnumConverter() },
-                DateTimeZoneHandling = DateTimeZoneHandling.Local
-            };
-            using (var scope = __serviceScopeFactory.CreateScope())
-            using (var db = scope.ServiceProvider.GetRequiredService<HealthChecksDb>())
-            {
-                var healthChecks = await db.Configurations.ToListAsync();
+            var data = await _caching.GetAsync("HealthChecks", async () =>
+              {
+                  var healthChecksExecutions = new List<HealthCheckExecution>();
+                  using (var scope = __serviceScopeFactory.CreateScope())
+                  using (var db = scope.ServiceProvider.GetRequiredService<HealthChecksDb>())
+                  {
+                      var healthChecks = await db.Configurations.ToListAsync();
 
-                var healthChecksExecutions = new List<HealthCheckExecution>();
 
-                foreach (var item in healthChecks.OrderBy(h => h.Id))
-                {
-                    var execution = await db.Executions
-                                .Include(le => le.Entries)
-                                .Where(le => le.Name == item.Name)
-                                .AsNoTracking()
-                                .SingleOrDefaultAsync();
-                    if (execution != null)
-                    {
-                        execution.History = await db.HealthCheckExecutionHistories
-                            .Where(eh => EF.Property<int>(eh, "HealthCheckExecutionId") == execution.Id)
-                            .OrderByDescending(eh => eh.On)
-                            .Take(10)
-                            .ToListAsync();
 
-                        healthChecksExecutions.Add(execution);
-                    }
-                }
-                var responseContent = JsonConvert.SerializeObject(healthChecksExecutions, _jsonSerializationSettings);
-              return   new JsonResult( healthChecksExecutions,_jsonSerializationSettings);
-            }
+                      foreach (var item in healthChecks.OrderBy(h => h.Id))
+                      {
+                          var execution = await db.Executions
+                                      .Include(le => le.Entries)
+                                      .Where(le => le.Name == item.Name)
+                                      .AsNoTracking()
+                                      .SingleOrDefaultAsync();
+                          if (execution != null)
+                          {
+                              execution.History = await db.HealthCheckExecutionHistories
+                                  .Where(eh => EF.Property<int>(eh, "HealthCheckExecutionId") == execution.Id)
+                                  .OrderByDescending(eh => eh.On)
+                                  .Take(10)
+                                  .ToListAsync();
+
+                              healthChecksExecutions.Add(execution);
+                          }
+                      }
+
+                  }
+                  return healthChecksExecutions;
+              }, TimeSpan.FromMinutes(10));
+            return new JsonResult(data.Value, _jsonSerializationSettings);
         }
     }
 }
