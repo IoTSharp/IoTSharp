52f795bd960cdf685602962f1d6958df7865704c
qian wang <1510106069@qq.com>
2022-12-10 22:45:11 +0800

    feat: 用户/客户/租户列表 去掉defineComponent包裹

--- ClientApp/src/views/iot/settings/tenantlist.vue
+++ ClientApp/src/views/iot/settings/tenantlist.vue
@@ -1,33 +1,24 @@
 <template>
-  <el-card>
-    <div class="z-crud">
-      <fs-crud ref="crudRef" v-bind="crudBinding"/>
-    </div>
-  </el-card>
+	<el-card>
+		<div class="z-crud">
+			<fs-crud ref="crudRef" v-bind="crudBinding" />
+		</div>
+	</el-card>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { useCrud } from '@fast-crud/fast-crud';
 import { useExpose } from '@fast-crud/fast-crud';
 import { createTenantListCrudOptions } from './crudOptions/tenantListCrudOptions';
-export default defineComponent({
-	name: 'TenantList',
-	setup() {
-		const crudRef = ref();
-		const crudBinding = ref();
-		const { crudExpose } = useExpose({ crudRef, crudBinding });
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-		const { crudOptions } = createTenantListCrudOptions({ crudExpose });
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-		useCrud({ crudExpose, crudOptions });
-		onMounted(() => {
-			crudExpose.doRefresh();
-		});
-		return {
-			crudBinding,
-			crudRef,
-		};
-	},
+const crudRef = ref();
+const crudBinding = ref();
+const { crudExpose } = useExpose({ crudRef, crudBinding });
+// eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
+const { crudOptions } = createTenantListCrudOptions({ crudExpose });
+// eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
+useCrud({ crudExpose, crudOptions });
+onMounted(() => {
+	crudExpose.doRefresh();
 });
 </script>
 
