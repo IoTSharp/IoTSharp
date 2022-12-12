14c1b72040a22cab98af11b94a73abc70f6acf3b
wq1234wq <wq1234wq@163.com>
2022-12-12 18:26:26 +0800

    审计日志查询

--- ClientApp/src/views/iot/rules/flowevents.vue
+++ ClientApp/src/views/iot/rules/flowevents.vue
@@ -6,17 +6,29 @@
           <el-row :gutter="35">
             <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
               <el-form-item label="事件名称">
-                <el-input v-model="query.CreatorName" placeholder="请输入事件名称"/>
+                <el-input v-model="query.Name" placeholder="请输入事件名称" />
               </el-form-item>
             </el-col>
             <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
               <el-form-item label="规则">
-                <el-input v-model="query.RuleId" placeholder="请选择规则"/>
+                <el-select v-model="query.RuleId" placeholder="请选择规则">
+                  <el-option v-for="item in state.rules" :key="item.value" :label="item.label" :value="item.value"
+                    :disabled="item.disabled" />
+                </el-select>
+
+
               </el-form-item>
             </el-col>
             <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
               <el-form-item label="创建对象">
-                <el-input v-model="query.Name" placeholder="请输入创建对象"/>
+
+
+
+                <el-select v-model="query.Creator"  filterable remote reserve-keyword placeholder="请输入创建对象名称"
+                  :remote-method="getCreators" :loading="state.creatorloading">
+                  <el-option v-for="item in state.creators" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+
               </el-form-item>
             </el-col>
           </el-row>
@@ -25,7 +37,7 @@
 
             <el-button size="default" type="primary" @click="getData()">
               <el-icon>
-                <ele-Search/>
+                <ele-Search />
               </el-icon>
               查询
             </el-button>
@@ -63,18 +75,17 @@
         <el-table-column label="操作" show-overflow-tooltip>
           <template #default="scope">
             <el-button size="small" text type="primary" v-if="
-            scope.row.alarmStatus === 'Active_UnAck' ||
-            scope.row.alarmStatus === 'Cleared_UnAck'
-          " @click="replay(scope.row)">回放
+              scope.row.alarmStatus === 'Active_UnAck' ||
+              scope.row.alarmStatus === 'Cleared_UnAck'
+            " @click="replay(scope.row)">回放
             </el-button>
           </template>
         </el-table-column>
       </el-table>
       <el-pagination @size-change="onHandleSizeChange" @current-change="onHandleCurrentChange" class="mt15"
-                     :pager-count="5" :page-sizes="[10, 20, 30]" v-model:current-page="state.tableData.param.pageNum"
-                     background
-                     v-model:page-size="state.tableData.param.pageSize" layout="total, sizes, prev, pager, next, jumper"
-                     :total="state.tableData.total">
+        :pager-count="5" :page-sizes="[10, 20, 30]" v-model:current-page="state.tableData.param.pageNum" background
+        v-model:page-size="state.tableData.param.pageSize" layout="total, sizes, prev, pager, next, jumper"
+        :total="state.tableData.total">
       </el-pagination>
 
     </component>
@@ -82,7 +93,7 @@
 </template>
 
 <script lang="ts" setup>
-import {ref, toRefs, reactive, onMounted, defineComponent} from "vue";
+import { ref, toRefs, reactive, onMounted, defineComponent } from "vue";
 import {
   ElMessageBox,
   ElMessage,
@@ -95,8 +106,9 @@ import {
   ElTableColumn,
 } from "element-plus";
 
-import {Session} from "/@/utils/storage";
-import {ruleApi} from "/@/api/flows";
+import { Session } from "/@/utils/storage";
+import { ruleApi } from "/@/api/flows";
+import { deviceApi } from "/@/api/devices";
 // 定义接口来定义对象的类型
 
 const props = defineProps({
@@ -126,10 +138,14 @@ interface TableDataRow {
 }
 
 interface TableDataState {
+  creatorloading: boolean;
+  creators: Array<any>;
+  rules: Array<any>
   tableData: {
     rows: Array<TableDataRow>;
     total: number;
     loading: boolean;
+
     param: {
       pageNum: number;
       pageSize: number;
@@ -138,8 +154,12 @@ interface TableDataState {
 }
 
 const userInfos = Session.get("userInfo");
+
 const router = useRouter();
 const state = reactive<TableDataState>({
+  creatorloading: false,
+  creators: [],
+  rules: [],
   tableData: {
     rows: [],
     total: 0,
@@ -152,11 +172,45 @@ const state = reactive<TableDataState>({
 });
 
 const query = reactive({
-  CreatorName: "",
+  Creator: "",
   Name: "",
   RuleId: "",
 });
 
+
+
+const getRules = () => {
+  ruleApi()
+    .ruleList({
+      limit: 100, offset: 0
+    })
+    .then((res) => {
+      state.rules = [...res.data.rows.map(c => { return { value: c.ruleId, label: c.name } })]
+    })
+}
+
+
+const getCreators = async (creator: string) => {
+  if (creator) {
+
+    const res = await deviceApi().devcieList({
+      offset: 0,
+      limit: 20,
+      onlyActive: false,
+      customerId: userInfos.customer.id,
+      name: creator
+    });
+
+    state.creators = [...res.data?.rows?.map(x => {
+      return {
+        label: x.name, value: x.name
+      }
+    })]
+  }
+
+
+
+}
 const replay = (val: TableDataRow) => {
 };
 const onHandleSizeChange = (val: number) => {
@@ -171,8 +225,7 @@ const onHandleCurrentChange = (val: number) => {
 };
 
 const getData = () => {
-
-  const params:{
+  const params: {
     offset: number
     limit: number
     Name: string
@@ -200,6 +253,8 @@ const initTableData = () => {
 };
 onMounted(() => {
   initTableData();
+
+  getRules();
 });
 
 
