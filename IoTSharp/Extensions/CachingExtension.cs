28aacbece2562271c16110a57efd7645226eb1d3
MysticBoy <mysticboy@live.com>
2023-12-03 08:27:06 +0800

    修正看板统计报错

--- IoTSharp/Extensions/CachingExtension.cs
+++ IoTSharp/Extensions/CachingExtension.cs
@@ -16,13 +16,13 @@ namespace IoTSharp.Extensions
             {
                 HomeKanbanDto m = new();
                 m.DeviceCount = _context.Device.Count(c => c.Tenant.Id == tid && !c.Deleted);
-                m.EventCount = _context.BaseEvents.Count(c => c.Tenant.Id == tid && c.CreaterDateTime > DateTime.Today && c.EventStaus > -1);
+                m.EventCount = _context.BaseEvents.Count(c => c.Tenant.Id == tid && c.CreaterDateTime > DateTime.Today.ToUniversalTime()   && c.EventStaus > -1);
                 var query = from c in _context.Device where c.Tenant.Id == tid && !c.Deleted select c;
                 var al = from a in _context.AttributeLatest where a.KeyName == Constants._Active && a.Value_Boolean == true select a.DeviceId;
                 var devquery = from x in query where al.Contains(x.Id) select x;
                 m.OnlineDeviceCount = devquery.Count();
-                m.AttributesDataCount = _context.AttributeLatest.Count(c => c.DateTime > DateTime.Today);
-                m.AlarmsCount = _context.Alarms.Count(c => c.Tenant.Id == tid && c.StartDateTime > DateTime.Today);
+                m.AttributesDataCount = _context.AttributeLatest.Count(c => c.DateTime > DateTime.Today.ToUniversalTime());
+                m.AlarmsCount = _context.Alarms.Count(c => c.Tenant.Id == tid && c.StartDateTime > DateTime.Today.ToUniversalTime());
                 var tuc = from t in _context.UserClaims where t.ClaimType == IoTSharpClaimTypes.Tenant select t;
                 var uq = from u in _context.Users where tuc.Any(c => c.UserId == u.Id) select u;
                 m.UserCount = uq.Count();
