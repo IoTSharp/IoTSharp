54fdb8d66e076fd0c4d82f8f5abdc6aaff46985a
maikebing <mysticboy@live.com>
2025-09-29 16:35:37 +0800

    修改标题 和描述等。

--- ClientApp/src/stores/themeConfig.ts
+++ ClientApp/src/stores/themeConfig.ts
@@ -137,11 +137,11 @@ export const useThemeConfig = defineStore('themeConfig', {
 			 * 全局网站标题 / 副标题
 			 */
 			// 网站主标题（菜单导航、浏览器当前网页标题）
-			globalTitle: 'vue-next-admin',
+			globalTitle: 'IoTSharp',
 			// 网站副标题（登录页顶部文字）
-			globalViceTitle: 'vueNextAdmin',
+			globalViceTitle: 'IoTSharp',
 			// 网站副标题（登录页顶部文字）
-			globalViceTitleMsg: '专注、免费、开源、维护、解疑',
+			globalViceTitleMsg: 'IoTSharp is an open-source IoT platform for data collection, processing, visualization, and device management.',
 			// 默认初始语言，可选值"<zh-cn|en|zh-tw>"，默认 zh-cn
 			globalI18n: 'zh-cn',
 			// 默认全局组件大小，可选值"<large|'default'|small>"，默认 'large'
